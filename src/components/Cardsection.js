fd2cc45910c2571a897eeacd569f1b5a1cf4bdb4
nada <nadasamir9334@gmail.com>
2023-08-21 19:07:16 -0400

    save all this

--- src/components/Cardsection.js
+++ src/components/Cardsection.js
@@ -6,11 +6,11 @@ function Cardsection({ id, name, source, img, category }) {
     return (
         <div>
             <div key={id} className="group relative">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <div className="aspect-h-1 aspect-w-1 w-72 overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-72">
                     <img
                         src={img}
                         alt=""
-                        className="h-full w-full object-cover object-center "
+                        className="h-72 w-72 object-cover object-center "
                     />
                 </div>
                 <div className="mt-4 flex justify-between">
