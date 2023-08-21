fd81ab7564293c465fb07f1068357ad11bd3483d
nada <nadasamir9334@gmail.com>
2023-08-21 14:24:18 -0400

    normal

--- src/pages/design/index.js
+++ src/pages/design/index.js
@@ -30,7 +30,7 @@ export default Landing
 
 
 export async function getStaticProps() {
-    const res = await fetch('https://newpodcast2.vercel.app/api/products');
+    const res = await fetch('http://localhost:3000/api/products');
     const data = await res.json();
 
 
