904658df86eac37c7c1ca650f60681cf9370f93b
nada <nadasamir9334@gmail.com>
2023-08-22 22:19:52 -0400

    save

--- src/pages/search.js
+++ src/pages/search.js
@@ -26,36 +26,41 @@ function search() {
                         onChange={(e) => handleChange(e.target.value)}
                     />
                 </div>
+                <div className=' container mx-auto px-4 py-5 sm:px-auto sm:py-5  lg:px-8'>
 
-                <div className={input !== "" ? 'block' : 'hidden'}>
+                    <div className={input !== "" ? 'block' : 'hidden'}>
 
-                    {products.filter((product) =>
-                        product.name.toLowerCase().includes(input)).map((product) => {
-                            return (
-                                <div className='container mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
-                                    <div className='grid grid-cols-1 sm:grid-cols-2'>
-                                        <div>
-                                            <img
-                                                src={product.img}
-                                                alt=""
-                                                className="h-20 w-20 object-cover object-center mx-auto "
-                                            />
+                        {products.filter((product) =>
+                            product.name.toLowerCase().includes(input)).map((product) => {
+                                return (
+                                    <>
+
+                                        <div className='grid grid-cols-1 sm:grid-cols-2 mx-auto px-4 py-5 sm:px-auto sm:py-5 '>
+                                            <div>
+                                                <img
+                                                    src={product.img}
+                                                    alt=""
+                                                    className="h-24 w-24 object-cover object-start mx-auto border"
+                                                />
+                                            </div>
+                                            <div>
+                                                <p className='text-md font-bold'>{product.name}</p>
+                                                <p className='text-sm'>{product.category}</p>
+                                            </div>
+
                                         </div>
-                                        <div>
-                                            <p className='text-md font-bold'>{product.name}</p>
-                                            <p className='text-sm'>{product.category}</p>
-                                        </div>
-
-                                    </div>
 
 
-                                </div>
+
+                                        <hr />
+                                    </>
+                                )
+                            }
+
                             )
                         }
 
-                        )
-                    }
-
+                    </div>
                 </div>
             </div>
 
