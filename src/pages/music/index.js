80c5b77b85a81b0ed7809e7dc7bf3105ed69fa2d
nada <nadasamir9334@gmail.com>
2023-08-23 21:34:17 -0400

    solve problem 2

--- src/pages/music/index.js
+++ src/pages/music/index.js
@@ -3,13 +3,13 @@ import Nav2 from '@/components/Nav2'
 import Songsection from '@/components/songsection'
 import { songs } from '../../../songs'
 
-function index({ songs }) {
+function index() {
     return (
         <>
             <Nav2 />
             <div>
                 <div className="mx-auto max-w-2xl px-10 py-10 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8">
-                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Design</h2>
+                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Song</h2>
 
                     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                         {songs.map((song) => {
