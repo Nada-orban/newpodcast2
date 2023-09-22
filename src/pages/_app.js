98ba57d148d71827d9b3b5b36067d64901f7704b
nada <nadasamir9334@gmail.com>
2023-09-21 22:58:16 -0400

    save with solve problem by prelineui

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react'
 import '@/styles/globals.css'
 import Nav1 from '@/components/Nav1'
 import Footer from '@/components/Footer'
@@ -7,8 +8,13 @@ import { store } from '../components/redux/store'
 // import { wrapper } from '../components/redux/Store'
 import Controls from '@/components/controls'
 import PlayerState from '@/components/context/PlayerState'
+'use client;'
+
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import('preline')
+  }, [])
   return (
     <Provider store={store}>
       {/* <PlayerState> */}
